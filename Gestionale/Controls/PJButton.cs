991d0a23b541be678b0e9571d7eb2cdc40f750b4
Michele Corazza <info@michelecorazza.name>
2022-09-23 22:51:25 +0200

    add custom button

---
+++ Gestionale/Controls/PJButton.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace Gestionale.Controls;
+
+public class PJButton : Button
+{
+	#region Fields
+	private int borderSize = 0;
+	private int borderRadius = 0;
+	private Color borderColor = Color.PaleVioletRed;
+	#endregion
+
+	#region Costructor
+	public PJButton()
+	{
+		FlatStyle = FlatStyle.Flat;
+		FlatAppearance.BorderSize = 0;
+		Size = new(150, 40);
+		BackColor = Color.MediumBlue;
+		ForeColor = Color.White;
+	}
+	#endregion
+
+	#region Methods
+	private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+	{
+		GraphicsPath path = new();
+		path.StartFigure();
+		path.AddArc(rect.X, rect.Y, radius, radius, 100, 90);
+		path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+		path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+		path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+		path.CloseFigure();
+
+		return path;
+    }
+
+	protected override void OnPaint(PaintEventArgs pevent)
+	{
+		base.OnPaint(pevent);
+		pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+		RectangleF rectSurface = new(0, 0, Width, Height);
+		RectangleF rectBorder = new(1, 1, Width - 0.8f, Height - 1);
+
+		if (borderRadius > 2)
+		{
+			// rounded button
+			using GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius);
+			using GraphicsPath pathBorder = GetFigurePath(rectSurface, borderRadius - 1);
+			using Pen penSurface = new(Parent.BackColor, 2);
+			using Pen penBorder = new(borderColor, borderSize);
+			
+			penBorder.Alignment = PenAlignment.Inset;
+			Region = new(pathSurface);
+
+			// draw border for HD result
+			pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+			// button border
+			if (borderSize >= 1)
+				// draw control border
+				pevent.Graphics.DrawPath(penBorder, pathBorder);
+		}
+		else
+		{
+			// normal button
+			Region = new(rectSurface);
+			if (borderSize >= 1)
+			{
+				using Pen penBorder = new(borderColor, borderSize);
+				penBorder.Alignment = PenAlignment.Inset;
+				pevent.Graphics.DrawRectangle(penBorder, 0, 0, Width - 1, Height - 1);
+
+            }
+
+		}
+    }
+
+
+	protected override void OnHandleCreated(EventArgs e)
+	{
+		base.OnHandleCreated(e);
+		Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+	}
+
+	private void Container_BackColorChanged(object? sender, EventArgs e)
+	{
+		if (DesignMode)
+			this.Invalidate();
+	}
+	#endregion
+}
--- Gestionale/Controls/PJTextBox.cs
+++ Gestionale/Controls/PJTextBox.cs
@@ -1,221 +1,220 @@
 ﻿using System.ComponentModel;
 
-namespace Gestionale.Controls
+namespace Gestionale.Controls;
+
+[DefaultEvent("_TextChanged")]
+public partial class PJTextBox : UserControl
 {
-    [DefaultEvent("_TextChanged")]
-    public partial class PJTextBox : UserControl
-    {
-        #region Fileds
-        private Color borderColor = Color.MediumSlateBlue;
-        private int borderSize = 2;
-        private bool underLinedStyle = false;
-        private Color borderFocusColor = Color.HotPink;
-        private bool isFocused = false;
+    #region Fileds
+    private Color borderColor = Color.MediumSlateBlue;
+    private int borderSize = 2;
+    private bool underLinedStyle = false;
+    private Color borderFocusColor = Color.HotPink;
+    private bool isFocused = false;
 
-        #endregion
+    #endregion
 
-        #region Properties
+    #region Properties
 
-        [Category("Pj Code Advance")]
-        public Color BorderColor 
-        { 
-            get => borderColor; 
-            set 
-            {
-                borderColor = value;
-                this.Invalidate();
-            } 
-        }
-
-        [Category("Pj Code Advance")]
-        public int BorderSize 
-        { 
-            get => borderSize; 
-            set
-            {
-                borderSize = value;
-                this.Invalidate();
-            }
-        }
-
-        [Category("Pj Code Advance")]
-        public bool UnderLinedStyle 
-        { 
-            get => underLinedStyle; 
-            set 
-            {
-                underLinedStyle = value;
-                this.Invalidate();
-            } 
-        }
-
-        [Category("Pj Code Advance")]
-        public bool PasswordChar 
-        { 
-            get => textBox1.UseSystemPasswordChar; 
-            set => textBox1.UseSystemPasswordChar = value; 
-        }
-
-        [Category("Pj Code Advance")]
-        public bool Multiline 
-        { 
-            get => textBox1.Multiline; 
-            set => textBox1.Multiline = value; 
-        }
-
-        [Category("Pj Code Advance")]
-        public override Color BackColor 
-        { 
-            get => base.BackColor; 
-            set
-            {
-                base.BackColor = value;
-                textBox1.BackColor = value;
-            }
-        }
-
-        [Category("Pj Code Advance")]
-        public override Color ForeColor 
-        { 
-            get => base.ForeColor; 
-            set
-            {
-                base.ForeColor = value;
-                textBox1.ForeColor = value;
-            }
-        }
-
-        [Category("Pj Code Advance")]
-        public override Font Font 
-        { 
-            get => base.Font; 
-            set
-            {
-                base.Font = value;
-                textBox1.Font = value;
-                if (DesignMode)
-                    UpdateControlHeight();
-            }
-        }
-
-        [Category("Pj Code Advance")]
-        public string Texts
-        { 
-            get => textBox1.Text; 
-            set => textBox1.Text = value; 
-        }
-
-        [Category("Pj Code Advance")]
-        public Color BorderFocusColor 
-        { 
-            get => borderFocusColor; 
-            set => borderFocusColor = value; 
-        }
-
-        #endregion
-
-        #region Costructor
-        public PJTextBox()
+    [Category("Pj Code Advance")]
+    public Color BorderColor 
+    { 
+        get => borderColor; 
+        set 
         {
-            InitializeComponent();
-        }
-        #endregion
+            borderColor = value;
+            this.Invalidate();
+        } 
+    }
 
-        #region Ovveridden methods
-
-        protected override void OnPaint(PaintEventArgs e)
+    [Category("Pj Code Advance")]
+    public int BorderSize 
+    { 
+        get => borderSize; 
+        set
         {
-            base.OnPaint(e);
-            Graphics graps = e.Graphics;
-
-            // draw border
-            using Pen penBorder = new(borderColor, borderSize);
-
-            penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
-
-            if (isFocused)
-                penBorder.Color = borderFocusColor;
-
-            if (underLinedStyle)
-                graps.DrawLine(penBorder, 0, Height - 1, Width, Height - 1);
-            else // normal style
-                graps.DrawRectangle(penBorder, 0, 0, Width - 0.5f, Height - 0.5f);
+            borderSize = value;
+            this.Invalidate();
         }
+    }
 
-        protected override void OnResize(EventArgs e)
+    [Category("Pj Code Advance")]
+    public bool UnderLinedStyle 
+    { 
+        get => underLinedStyle; 
+        set 
         {
-            base.OnResize(e);
-            if(DesignMode)
+            underLinedStyle = value;
+            this.Invalidate();
+        } 
+    }
+
+    [Category("Pj Code Advance")]
+    public bool PasswordChar 
+    { 
+        get => textBox1.UseSystemPasswordChar; 
+        set => textBox1.UseSystemPasswordChar = value; 
+    }
+
+    [Category("Pj Code Advance")]
+    public bool Multiline 
+    { 
+        get => textBox1.Multiline; 
+        set => textBox1.Multiline = value; 
+    }
+
+    [Category("Pj Code Advance")]
+    public override Color BackColor 
+    { 
+        get => base.BackColor; 
+        set
+        {
+            base.BackColor = value;
+            textBox1.BackColor = value;
+        }
+    }
+
+    [Category("Pj Code Advance")]
+    public override Color ForeColor 
+    { 
+        get => base.ForeColor; 
+        set
+        {
+            base.ForeColor = value;
+            textBox1.ForeColor = value;
+        }
+    }
+
+    [Category("Pj Code Advance")]
+    public override Font Font 
+    { 
+        get => base.Font; 
+        set
+        {
+            base.Font = value;
+            textBox1.Font = value;
+            if (DesignMode)
                 UpdateControlHeight();
-            
         }
+    }
 
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
+    [Category("Pj Code Advance")]
+    public string Texts
+    { 
+        get => textBox1.Text; 
+        set => textBox1.Text = value; 
+    }
+
+    [Category("Pj Code Advance")]
+    public Color BorderFocusColor 
+    { 
+        get => borderFocusColor; 
+        set => borderFocusColor = value; 
+    }
+
+    #endregion
+
+    #region Costructor
+    public PJTextBox()
+    {
+        InitializeComponent();
+    }
+    #endregion
+
+    #region Ovveridden methods
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        Graphics graps = e.Graphics;
+
+        // draw border
+        using Pen penBorder = new(borderColor, borderSize);
+
+        penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+        if (isFocused)
+            penBorder.Color = borderFocusColor;
+
+        if (underLinedStyle)
+            graps.DrawLine(penBorder, 0, Height - 1, Width, Height - 1);
+        else // normal style
+            graps.DrawRectangle(penBorder, 0, 0, Width - 0.5f, Height - 0.5f);
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+        if(DesignMode)
             UpdateControlHeight();
-        }
-
         
+    }
 
-        #endregion
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        UpdateControlHeight();
+    }
 
-        #region Private methods
-        private void UpdateControlHeight()
+    
+
+    #endregion
+
+    #region Private methods
+    private void UpdateControlHeight()
+    {
+        if (!textBox1.Multiline)
         {
-            if (!textBox1.Multiline)
-            {
-                int txtHeight = TextRenderer.MeasureText("Text", Font).Height + 1;
-                textBox1.Multiline = true;
-                textBox1.MinimumSize = new(0, txtHeight);
-                textBox1.Multiline = false;
+            int txtHeight = TextRenderer.MeasureText("Text", Font).Height + 1;
+            textBox1.Multiline = true;
+            textBox1.MinimumSize = new(0, txtHeight);
+            textBox1.Multiline = false;
 
-                Height = textBox1.Height + Padding.Top + Padding.Bottom;
-            }
+            Height = textBox1.Height + Padding.Top + Padding.Bottom;
         }
-        #endregion
+    }
+    #endregion
 
-        #region Events
+    #region Events
 
-        public event EventHandler _TextChanged;
-        #endregion
+    public event EventHandler _TextChanged;
+    #endregion
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if (_TextChanged is not null)
-                _TextChanged.Invoke(sender, e);
-        }
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        if (_TextChanged is not null)
+            _TextChanged.Invoke(sender, e);
+    }
 
-        private void textBox1_Click(object sender, EventArgs e)
-        {
-            OnClick(e);
-        }
+    private void textBox1_Click(object sender, EventArgs e)
+    {
+        OnClick(e);
+    }
 
-        private void textBox1_MouseEnter(object sender, EventArgs e)
-        {
-            OnMouseEnter(e);
-        }
+    private void textBox1_MouseEnter(object sender, EventArgs e)
+    {
+        OnMouseEnter(e);
+    }
 
-        private void textBox1_MouseLeave(object sender, EventArgs e)
-        {
-            OnMouseLeave(e);
-        }
+    private void textBox1_MouseLeave(object sender, EventArgs e)
+    {
+        OnMouseLeave(e);
+    }
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            OnKeyPress(e);
-        }
+    private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        OnKeyPress(e);
+    }
 
-        private void textBox1_Enter(object sender, EventArgs e)
-        {
-            isFocused = true;
-            this.Invalidate();
-        }
+    private void textBox1_Enter(object sender, EventArgs e)
+    {
+        isFocused = true;
+        this.Invalidate();
+    }
 
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-            isFocused = false;
-            this.Invalidate();
-        }
+    private void textBox1_Leave(object sender, EventArgs e)
+    {
+        isFocused = false;
+        this.Invalidate();
     }
 }
