a78ad8b6869c6c36b2a6c443f72914c91d6f90b0
Michele Corazza <info@michelecorazza.name>
2022-09-23 21:53:14 +0200

    Add custom textbox

---
+++ Gestionale/Controls/PJTextBox.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Gestionale.Controls
+{
+    partial class PJTextBox
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(7, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 17);
+            this.textBox1.TabIndex = 0;
+            // 
+            // PJTextBox
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PJTextBox";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(250, 30);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
---
+++ Gestionale/Controls/PJTextBox.cs
@@ -0,0 +1,98 @@
+﻿namespace Gestionale.Controls
+{
+    public partial class PJTextBox : UserControl
+    {
+        #region Fileds
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underLinedStyle = false;
+
+        #endregion
+
+        #region Properties
+        public Color BorderColor 
+        { 
+            get => borderColor; 
+            set 
+            {
+                borderColor = value;
+                this.Invalidate();
+            } 
+        }
+        public int BorderSize 
+        { 
+            get => borderSize; 
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+        public bool UnderLinedStyle 
+        { 
+            get => underLinedStyle; 
+            set 
+            {
+                underLinedStyle = value;
+                this.Invalidate();
+            } 
+        }
+        #endregion
+
+        #region Costructor
+        public PJTextBox()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Ovveridden methods
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graps = e.Graphics;
+
+            // draw border
+            using Pen penBorder = new(borderColor, borderSize);
+
+            penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+            if (underLinedStyle)
+                graps.DrawLine(penBorder, 0, Height - 1, Width, Height - 1);
+            else // normal style
+                graps.DrawRectangle(penBorder, 0, 0, Width - 0.5f, Height - 0.5f);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if(DesignMode)
+                UpdateControlHeight();
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        #endregion
+
+        #region Private methods
+        private void UpdateControlHeight()
+        {
+            if (!textBox1.Multiline)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new(0, txtHeight);
+                textBox1.Multiline = false;
+
+                Height = textBox1.Height + Padding.Top + Padding.Bottom;
+            }
+        }
+        #endregion
+    }
+}
